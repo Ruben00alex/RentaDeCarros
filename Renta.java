b06e1a6640ded2536887b5cc485d1d2ac252c375
Alex Ramos <ruben.00alex@gmail.com>
2021-08-20 07:45:43 +0000

    Se añadió Renta

---
+++ Renta.java
@@ -0,0 +1,18 @@
+public class Renta{
+  Cliente cliente;
+  Carro carroEnRenta;
+  
+
+  public Renta(Cliente cliente, Carro carro){
+    this.cliente = cliente;
+    carroEnRenta = carro;
+  }
+
+
+  //métodos:
+
+
+
+
+
+}
